970f6070a6fc295c9047ef7986fc3623cadfc859
rexlypenafloridaii <rpenafloridaii@gmail.com>
2024-02-26 19:32:46 -0600

    Add routes for all products and an invidivual product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def full_list
+    @products = Product.all
+    render template: "products/index"
+  end
+
+  def just_one
+    @product = Product.first
+    render template: "product/show"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/full", controller: "products", action: "full_list"
+  get "/one", controller: "products", action: "just_one"
 end
