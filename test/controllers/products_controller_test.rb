3e4222a57bbcf35028fa20214219a221c3e62afc
rexlypenafloridaii <rpenafloridaii@gmail.com>
2024-03-20 20:29:41 -0500

    Add admin authentication in tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,6 +4,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @user = User.create(name: "Admin", email: "admin@example.com", password: "password", admin: true)
+    post "/sessions.json", params: { email: "admin@example.com", password: "password" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
 
   test "index" do
     get "/products.json"
@@ -15,11 +21,18 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { supplier_id: Supplier.first.id, name: "test name", price: 1, description: "test description", inventory: 1 }
+      post "/products.json",
+        headers: { "Authorization" => "Bearer #{@jwt}" },
+        params: { supplier_id: Supplier.first.id, name: "test name", price: 1, description: "test description", inventory: 1 }
+      data = JSON.parse(response.body)
+      assert_response 200
+      refute_nil data["id"]
     end
 
     assert_difference "Product.count", 0 do
-      post "/products.json", params: {}
+      post "/products.json",
+        headers: { "Authorization" => "Bearer #{@jwt}" },
+        params: {}
       data = JSON.parse(response.body)
       assert_response 422
     end
@@ -35,19 +48,23 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "update" do
     product = Product.first
-    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    patch "/products/#{product.id}.json",
+      headers: { "Authorization" => "Bearer #{@jwt}" },
+      params: { name: "Updated name" }
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
 
-    patch "/products/#{product.id}.json", params: { price: -100 }
+    patch "/products/#{product.id}.json",
+      headers: { "Authorization" => "Bearer #{@jwt}" },
+      params: { price: -100 }
     assert_response 422
   end
 
   test "destroy" do
     assert_difference "Product.count", -1 do
-      delete "/products/#{Product.first.id}.json"
+      delete "/products/#{Product.first.id}.json", headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
   end
